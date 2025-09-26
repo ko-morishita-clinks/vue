9326967c433d4d4d501d5a27fa0d22a869b41a94
ko-morishita-clinks <ko-morishita@clinks.jp>
2025-09-26 18:24:59 +0900

    feat: sec9

--- section9/vuex/src/components/HelloWorld.vue
+++ section9/vuex/src/components/HelloWorld.vue
@@ -1,14 +1,39 @@
 <template>
   <div>
-
+    <button @click="incrementAction">+</button>
+    <button @click="addCount">+10</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+
 export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  methods:{
+    // increment(){
+    //   this.$store.commit('increment');
+    // },
+    // addCount(){
+    //   this.$store.commit('addCount', {
+    //     value: 10
+    //   });
+    // },
+    // increment(){
+    //   this.$store.dispatch('incrementAction')
+    // },
+    // addCount(){
+    //   this.$store.dispatch('addCountAction', { value: 10 })
+    // },
+    ...mapActions(['incrementAction', 'addCountAction']),
+    addCount(){
+      this.addCountAction( {
+        value: 10
+      })
+    }
   }
 }
 </script>
--- section9/vuex/src/store/index.js
+++ section9/vuex/src/store/index.js
@@ -1,17 +1,45 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import auth from './auth'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 0,
+    users:[
+      { id: 1, name: '大谷', isVisible: true },
+      { id: 2, name: 'ダルビッシュ', isVisible: true },
+      { id: 3, name: '菊池', isVisible: true },
+    ]
   },
   getters: {
+    // visibleUsers( state ){
+    //   return state.users.filter( user => user.isVisible )
+    // },
+    visibleUsers: state => state.users.filter( user => user.isVisible ),
+
+    getUserById: state => id => {
+      return state.users.find( user => user.id === id )
+    }
   },
   mutations: {
+    increment( state ){
+      state.count++
+    },
+    addCount( state, payload ){
+      state.count += payload.value
+    }
   },
   actions: {
+    incrementAction({ commit }){
+      commit('increment')
+    },
+    addCountAction( { commit }, payload ){
+      commit('addCount', payload)
+    },
   },
   modules: {
+    auth
   }
 })
